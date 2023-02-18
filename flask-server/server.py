087b42b7fe559e90edaa873a9c5cc4bf61978947
cyonas <cyonas@scu.edu>
2023-02-18 15:47:10 -0800

    flask server and react client

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
+
+app = Flask(__name__)
 
 site = '01646500'
 start_date='2022-02-18'
 end_date= '2023-02-13'
-app = Flask(__name__)
+
 
 url = f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={site}&startDT={start_date}&endDT={end_date}&parameterCd=00400'
 
@@ -17,7 +19,8 @@ ph_values = [x["value"][0]["value"] for x in data["value"]["timeSeries"][1]["val
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3", ph_values]}
+    return {"members": ["Give me the goats name in JSON:", "Will Cockrum", "Oh also whats the PH levels of water in Columbus, Ohio:", ph_values]}
+    #return "<div>Hello World</div>"
 
 if __name__ == "__main__":
     app.run(debug=True)
