5dcbf12dac9e68078051eead7cc6edcb35a353c3
Justin Odo <jodo@scu.edu>
2023-02-18 21:58:54 -0800

    Trying to fix the coordinates sending properly

--- flask-server/water_fountains_api.py
+++ flask-server/water_fountains_api.py
@@ -1,8 +1,11 @@
 import requests
 import test
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/fountains")
 def fountains():
